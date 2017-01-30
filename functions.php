e612497feeb4e97f58e07da4021ec38e1d9f3c35
Václav Černý <vatoz@seznam.cz>
2017-01-30 12:27:17 +0100

    Czech translation

--- functions.php
+++ functions.php
@@ -8,3 +8,9 @@ function wpimprov_vantage_child_enqueue_parent_style() {
     wp_enqueue_style( 'vantage-parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'wpimprov_vantage_child_enqueue_parent_style', 8 );
+
+// Load translation files from your child theme instead of the parent theme
+function wpimprov_vantage_child_locale() {
+    load_child_theme_textdomain( 'vantage', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'wpimprov_vantage_child_locale' );
